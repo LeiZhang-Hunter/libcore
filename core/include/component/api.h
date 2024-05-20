fb03173ff87d0e10c94ae6129f6cd4e577d4f395
LeiZhang-Hunter <357733652@qq.com>
2024-05-20 22:20:40 +0800

    fix: api.h

--- core/include/component/api.h
+++ core/include/component/api.h
@@ -64,27 +64,51 @@ public:
     std::string message_;
 };
 
+class Data {
+public:
+    virtual void* data() PURE;
+};
+
+
+template<class T>
+class AnyData :public Data {
+public:
+    AnyData(std::unique_ptr<T> data) {
+        data_ = std::move(data);
+    }
+    virtual void* data() override {
+        return data_.get();
+    };
+private:
+    std::unique_ptr<T> data_;
+};
+
 class EventData {
 public:
     virtual std::map<std::string, std::string> meta() PURE;
     virtual std::map<std::string, std::string> header() PURE;
     virtual void* data() PURE;
+    virtual std::unique_ptr<Data>& dataPtr() PURE;
     virtual std::string name() PURE;
     // Fill event with meta,header,body cannot be nil
-    virtual void  fill(const std::map<std::string, std::string>& meta, const std::map<std::string, std::string>& header, void* data) PURE;
+    virtual void  fill(const std::map<std::string, std::string>& meta, const std::map<std::string, std::string>& header, std::unique_ptr<Data> data) PURE;
     virtual void release() PURE;
 };
 
 class Batch :public Nonmoveable, Noncopyable {
 public:
     virtual std::map<std::string, std::string> meta() PURE;
-    virtual std::vector<std::shared_ptr<Core::Component::EventData>>& events() PURE;
+    virtual std::vector<std::unique_ptr<Core::Component::EventData>>& events() PURE;
+    virtual void fill(std::map<std::string, std::string> meta, std::vector<std::unique_ptr<Core::Component::EventData>>& events) PURE;
     virtual void Release() PURE;
 };
 
+class Pipeline;
+
 class Queue :public Nonmoveable, Noncopyable{
 public:
-    virtual void product(const std::shared_ptr<EventData>& e) PURE;
+    virtual void run() PURE;
+    virtual void stop() PURE;
 };
 
 
@@ -96,7 +120,27 @@ public:
 
     virtual bool bindChannel(std::shared_ptr<Queue> channel) PURE;
 
-    virtual Result Consume(Batch& batch) PURE;
+    virtual Result  Consume(Batch& batch) PURE;
+};
+
+class Interceptor {
+public:
+    virtual Core::Component::Result intercept(std::unique_ptr<Core::Component::Batch>& batch) PURE;
+
+    virtual Core::Component::Result intercept(Core::Component::Batch& batch) PURE;
+};
+
+class Pipeline {
+public:
+    virtual void addIntercept(std::unique_ptr<Interceptor>& interceptor) PURE;
+
+    virtual void addConsumer(std::unique_ptr<Consumer>& consumer) PURE;
+
+    virtual void bindChannel(std::shared_ptr<Queue>& queue) PURE;
+
+    virtual void push(std::unique_ptr<EventData> e) PURE;
+
+    virtual void flush() PURE;
 };
 
 }
